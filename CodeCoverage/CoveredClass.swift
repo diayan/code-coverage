15d5e4d2e36c735c7231fc5c9f7c1182a656675a
diayan <diayans@outlook.com>
2022-03-03 23:20:32 +0000

    add new function that has a loop

--- CodeCoverage/CoveredClass.swift
+++ CodeCoverage/CoveredClass.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class CoveredClass {
+    
+    static func max(_ x: Int, _ y: Int) -> Int {
+        if x < y {
+            return y
+        }else {
+            return x
+        }
+    }
+    
+    static func commaSeparated(from: Int, to: Int) -> String {
+        var result = ""
+        
+        for i in from..<to {
+            result += "\(i)"
+        }
+        result += "\(to)"
+        return result
+    }
+}
--- CodeCoverageTests/CoveredClassTests.swift
+++ CodeCoverageTests/CoveredClassTests.swift
@@ -6,30 +6,17 @@
 //
 
 import XCTest
+@testable import CodeCoverage
 
 class CoveredClassTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_max_with1And2_shouldReturn2() {
+        let result = CoveredClass.max(1, 2)
+        XCTAssertEqual(result, 2)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_max_with1And2_shouldReturn3() {
+        let result = CoveredClass.max(3, 2)
+        XCTAssertEqual(result, 3)
     }
 
 }
